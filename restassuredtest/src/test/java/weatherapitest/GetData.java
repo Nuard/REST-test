1c705cef5897e7210468ae424c1652b629a637b4
Nuard <nuard@atabekyan.com>
2018-07-12 22:46:19 +0400

    updates

--- restassuredtest/src/test/java/weatherapitest/GetData.java
+++ restassuredtest/src/test/java/weatherapitest/GetData.java
@@ -34,6 +34,12 @@ public class GetData extends TestBase {
     }
 
     @Test
+    public void testGetReqResBody() {
+
+        given().when().get("https://reqres.in/api/users").getBody().prettyPrint();
+    }
+
+    @Test
     public void testPost() {
         RequestSpecification request = RestAssured.given();
         request.header("Content-Type","application/json");
@@ -50,7 +56,34 @@ public class GetData extends TestBase {
         Assert.assertEquals(code, 201);
 
         response.prettyPrint();
+    }
 
+        @Test
+    public void testDelete() {
+        RequestSpecification request = RestAssured.given();
+        Response response = request.delete("https://reqres.in/api/users/2");
+
+        int code = response.getStatusCode();
+        Assert.assertEquals(code, 204);
+        }
+
+        @Test
+    public void testPut() {
+            RequestSpecification request = RestAssured.given();
+            request.header("Content-Type","application/json");
+
+            JSONObject json = new JSONObject();
+            json.put("name","morpheus");
+            json.put("job","zion resident");
+
+            request.body(json.toJSONString());
+
+            Response response = request.put("https://reqres.in/api/users/2");
+
+            int code = response.getStatusCode();
+            Assert.assertEquals(code, 200);
+
+            response.prettyPrint();
     }
 }
 
